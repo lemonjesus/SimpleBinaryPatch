17b2f870e4f7944189725f1b364a524258be5d0f
Wesley Van Pelt <wcvp@me.com>
2020-10-25 16:45:19 -0500

    added new files

---
+++ bin_patch.c
@@ -0,0 +1,13 @@
+#include "bin_patch.h"
+
+
+enum bp_ret_code gen_patch(uint8_t *old, uint8_t *new, struct binary_patch *patch)
+{
+	return SUCCESS;
+}
+
+
+enum bp_ret_code apply_patch(uint8_t *dest, struct binary_patch *patch)
+{
+	return SUCCESS;
+}
