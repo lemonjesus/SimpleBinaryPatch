5b4030262ffd4ff1894cea16340db281fd9b8a67
Wesley Van Pelt <wcvp@me.com>
2020-10-25 18:36:17 -0500

    finished bin_patch implementation

--- bin_patch.c
+++ bin_patch.c
@@ -22,21 +22,27 @@ enum bp_ret_code gen_patch(uint8_t *old, uint8_t *new, struct binary_patch *patc
 		patch->diff_start[curr_diff] = i;
 
 		for (; i < patch->bin_len && old[i] != new[i]; i++);
-		if (i - patch->diff_start[curr_diff] > patch->heap_len - curr_heap)
+		patch->diff_delta[curr_diff] = i - patch->diff_start[curr_diff];
+		if (patch->diff_delta[curr_diff] > patch->heap_len - curr_heap)
 			return OUT_OF_HEAP;
-		patch->diff_stop[curr_diff] = i;
 
 		blockcpy(&patch->heap[curr_heap],
 		         &new[patch->diff_start[curr_diff]],
-		         patch->diff_stop[curr_diff] - patch->diff_start[curr_diff]);
-		curr_heap += patch->diff_stop[curr_diff] - patch->diff_start[curr_diff];
+		         patch->diff_delta[curr_diff]);
+		curr_heap += patch->diff_delta[curr_diff];
 	}
 
 	return OUT_OF_DIFF;
 }
 
 
-enum bp_ret_code apply_patch(uint8_t *dest, struct binary_patch *patch)
+void apply_patch(uint8_t *dest, struct binary_patch *patch)
 {
-	return SUCCESS;
+	len_t curr_heap = 0;
+	for (len_t i = 0; i < patch->diff_len; i++) {
+		blockcpy(&dest[patch->diff_start[i]],
+		         &patch->heap[curr_heap],
+		         patch->diff_delta[i]);
+		curr_heap += patch->diff_delta[i];
+	}
 }
